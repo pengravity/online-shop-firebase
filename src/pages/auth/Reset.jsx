44dad3be065ff0dcb651cff4c34041f6b1a080c6
Andy <alipinski@mail.com>
2022-11-02 18:20:49 +0100

    reset password

--- src/pages/auth/Reset.jsx
+++ src/pages/auth/Reset.jsx
@@ -1,35 +1,67 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../../firebase/config';
 
+import { toast } from 'react-toastify';
 import Card from '../../components/card/Card';
+import Spinner from '../../components/spinner/Spinner';
 
 import './auth.scss';
 
 const Reset = () => {
+  const [email, setEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const resetPassword = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        toast.success('Reset password email sent ');
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        toast.error(error.message);
+        setIsLoading(false);
+      });
+  };
+
   return (
-    <section className='container auth'>
-      <Card>
-        <div className='form'>
-          <h2>Reset Password</h2>
+    <>
+      {isLoading && <Spinner />}
+      <section className='container auth'>
+        <Card>
+          <div className='form'>
+            <h2>Reset Password</h2>
 
-          <form>
-            <input type='text' placeholder='Email' required />
+            <form onSubmit={resetPassword}>
+              <input
+                type='text'
+                placeholder='Email'
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
 
-            <button className='--btn --btn-primary --btn-block'>
-              Reset Password
-            </button>
+              <button type='submit' className='--btn --btn-primary --btn-block'>
+                Reset Password
+              </button>
 
-            <span className='links'>
-              <p>
-                <Link to='/login'>Login</Link>
-              </p>
-              <p>
-                <Link to='/register'>Register</Link>
-              </p>
-            </span>
-          </form>
-        </div>
-      </Card>
-    </section>
+              <span className='links'>
+                <p>
+                  <Link to='/login'>Login</Link>
+                </p>
+                <p>
+                  <Link to='/register'>Register</Link>
+                </p>
+              </span>
+            </form>
+          </div>
+        </Card>
+      </section>
+    </>
   );
 };
 
