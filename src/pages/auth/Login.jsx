dcada2e52d342fad582d5c8ab23be7433de20335
Andy <alipinski@mail.com>
2022-11-02 11:04:10 +0100

    card and register components

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,30 +1,35 @@
 import { Link } from 'react-router-dom';
 import { GrGoogle } from 'react-icons/gr';
 
+import Card from '../../components/card/Card';
 import './auth.scss';
 
 const Login = () => {
   return (
     <section className='container auth'>
-      <div className='form'>
-        <h2>Login</h2>
-        <form>
-          <input type='text' placeholder='Email' required />
-          <input type='password' placeholder='Password' required />
-          <button className='--btn --btn-primary --btn-block'>Login</button>
-          <div className='links'>
-            <Link to='/reset'>Reset Password</Link>
-          </div>
-          <p>-- or --</p>
-        </form>
-        <button className='--btn --btn-danger --btn-block'>
-          <GrGoogle /> &nbsp; Login With Google
-        </button>
-        <span className='register'>
-          <p className='black'>Don't have an account?</p>&nbsp;
-          <Link to='/register'>Register</Link>
-        </span>
-      </div>
+      <Card>
+        <div className='form'>
+          <h2>Login</h2>
+
+          <form>
+            <input type='text' placeholder='Email' required />
+            <input type='password' placeholder='Password' required />
+            <button className='--btn --btn-primary --btn-block'>Login</button>
+            <div className='links'>
+              <Link to='/reset'>Reset Password</Link>
+            </div>
+            <p>-- or --</p>
+          </form>
+
+          <button className='--btn --btn-danger --btn-block'>
+            <GrGoogle /> &nbsp; Login With Google
+          </button>
+          <span className='register'>
+            <p className='black'>Don't have an account?</p>&nbsp;
+            <Link to='/register'>Register</Link>
+          </span>
+        </div>
+      </Card>
     </section>
   );
 };
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,7 +1,34 @@
+import { Link } from 'react-router-dom';
+import { GrGoogle } from 'react-icons/gr';
+
+import Card from '../../components/card/Card';
+
 import './auth.scss';
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className='container auth'>
+      <Card>
+        <div className='form'>
+          <h2>Register</h2>
+
+          <form>
+            <input type='text' placeholder='Email' required />
+            <input type='password' placeholder='Password' required />
+            <input type='password' placeholder='Confirm Password' required />
+            <button className='--btn --btn-primary --btn-block'>
+              Register
+            </button>
+          </form>
+
+          <span className='register'>
+            <p className='black'>Have an account?</p>&nbsp;
+            <Link to='/login'>Login</Link>
+          </span>
+        </div>
+      </Card>
+    </section>
+  );
 };
 
 export default Register;
