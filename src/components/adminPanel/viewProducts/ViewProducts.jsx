55be1e50783e2180f44f05b359ab7089d93b0ee3
Andy <alipinski@mail.com>
2022-11-07 14:26:18 +0100

    getting products  data from firestore

--- src/components/adminPanel/viewProducts/ViewProducts.jsx
+++ src/components/adminPanel/viewProducts/ViewProducts.jsx
@@ -1,6 +1,48 @@
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import {
+  collection,
+  doc,
+  setDoc,
+  query,
+  where,
+  onSnapshot,
+  orderBy,
+} from 'firebase/firestore';
+
+import { db } from '../../../firebase/config';
 import styles from './ViewProducts.module.scss';
 
 const ViewProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getProducs();
+  }, []);
+
+  const getProducs = () => {
+    setIsLoading(true);
+
+    try {
+      const productsRef = collection(db, 'products');
+      const q = query(productsRef, orderBy('createdAt', 'desc'));
+
+      onSnapshot(q, (snapshot) => {
+        console.log(snapshot.docs);
+        const allProducts = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log(allProducts);
+        setProducts(allProducts);
+      });
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div>
       <h1>View products </h1>
