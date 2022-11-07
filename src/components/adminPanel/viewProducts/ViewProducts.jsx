1266e5f57c32c93d2c835433d0a033069424723e
Andy <alipinski@mail.com>
2022-11-07 17:08:21 +0100

    display products on the page

--- src/components/adminPanel/viewProducts/ViewProducts.jsx
+++ src/components/adminPanel/viewProducts/ViewProducts.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import {
   collection,
@@ -9,6 +10,7 @@ import {
   onSnapshot,
   orderBy,
 } from 'firebase/firestore';
+import { FaEdit, FaTrashAlt } from 'react-icons/fa';
 
 import { db } from '../../../firebase/config';
 import styles from './ViewProducts.module.scss';
@@ -29,13 +31,13 @@ const ViewProducts = () => {
       const q = query(productsRef, orderBy('createdAt', 'desc'));
 
       onSnapshot(q, (snapshot) => {
-        console.log(snapshot.docs);
         const allProducts = snapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
         }));
         console.log(allProducts);
         setProducts(allProducts);
+        setIsLoading(false);
       });
     } catch (error) {
       setIsLoading(false);
@@ -44,9 +46,53 @@ const ViewProducts = () => {
   };
 
   return (
-    <div>
-      <h1>View products </h1>
-    </div>
+    <>
+      <div className={styles.table}>
+        <h2>All products </h2>
+        {products.length === 0 ? (
+          <p>No products found...</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>Number</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Price</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, index) => {
+                const { id, name, imageURL, price, category } = product;
+                return (
+                  <tr key={id}>
+                    <td>{index + 1}</td>
+                    <td>
+                      <img
+                        src={imageURL}
+                        alt={name}
+                        style={{ width: '120px', height: '120px' }}
+                      />
+                    </td>
+                    <td>{name}</td>
+                    <td>{category}</td>
+                    <td>${price}</td>
+                    <td>
+                      <Link to='/admin/add-product'></Link>
+                      <FaEdit size={18} color='green' />
+                      &nbsp;
+                      <FaTrashAlt size={16} color='red' />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </>
   );
 };
 
