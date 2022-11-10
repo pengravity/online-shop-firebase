83655872c1ef0e802704d72e6ac51ce63ecebd84
Andy <alipinski@mail.com>
2022-11-10 14:39:01 +0100

    made createdAt serializable

--- src/components/adminPanel/addProducts/AddProducts.jsx
+++ src/components/adminPanel/addProducts/AddProducts.jsx
@@ -77,7 +77,7 @@ const AddProducts = () => {
         category: product.category,
         brand: product.brand,
         desc: product.desc,
-        createdAt: Timestamp.now().toDate(),
+        createdAt: Timestamp.now().toDate().toString(),
       });
       setIsLoading(false);
       setUploadProgress(0);
