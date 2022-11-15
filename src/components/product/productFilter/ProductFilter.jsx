e4044b71e5d8a4a1314a3bc01fbc06430ac6dabd
Andy <alipinski@mail.com>
2022-11-15 11:18:19 +0100

    filtering products by category

--- src/components/product/productFilter/ProductFilter.jsx
+++ src/components/product/productFilter/ProductFilter.jsx
@@ -1,11 +1,44 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { FILTER_BY_CATEGORY } from '../../../redux/slices/filterSlice';
+import { selectProducts } from '../../../redux/slices/productSlice';
 import styles from './ProductFilter.module.scss';
 
 const ProductFilter = () => {
+  const [category, setCategory] = useState('All');
+  const products = useSelector(selectProducts);
+
+  const dispatch = useDispatch();
+
+  const allCategories = [
+    'All',
+    ...new Set(products.map((product) => product.category)),
+  ];
+  // console.log(allCategories);
+
+  const filterProducts = (categ) => {
+    setCategory(categ);
+    dispatch(FILTER_BY_CATEGORY({ products, category: categ }));
+  };
+
   return (
     <div className={styles.filter}>
       <h4>Categories</h4>
       <div className={styles.category}>
-        <button>All</button>
+        {allCategories.map((categ, index) => {
+          return (
+            <button
+              key={index}
+              type='button'
+              className={`${category}` === categ ? `${styles.active}` : null}
+              onClick={() => filterProducts(categ)}
+            >
+              {categ}
+            </button>
+          );
+        })}
+        {/* <button>All</button> */}
       </div>
       <h4>Price</h4>
       <p>100</p>
